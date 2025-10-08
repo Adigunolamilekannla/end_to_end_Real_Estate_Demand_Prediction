9ff10489d4eb81236f7f419d9ac3f3c5a9127801
leksman <chadjeffror123@gmail.com>
2025-10-08 20:03:45 +0100

    Final Changes Commited

--- application.py
+++ application.py
@@ -98,4 +98,4 @@ def predict():
 # Run app (for local and Elastic Beanstalk)
 # ==================================================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=False)
